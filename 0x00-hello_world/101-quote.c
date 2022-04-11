9cdaa0526673c3c802e13dc49b4f68ba92591a70
k1238 <kalebadino@gmail.com>
2022-04-11 10:56:13 -0700

    ay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 #include <unistd.h>
 
@@ -11,9 +12,4 @@ int main(void)
 write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 return (1);
 }
-
-
-~
-~
-~
 
