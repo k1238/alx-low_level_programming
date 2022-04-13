f65bbb2db6f6737d7b689a905b4b2f019b31386b
k1238 <kalebadino@gmail.com>
2022-04-13 13:00:26 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,12 @@
 #include  <stdio.h>
 
 /**
- * mian - Prints the first 98 Fibonacci numbers, starting with
- * 1 and 2, separated by a comma follwed by a space.
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ * 1 and 2, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
