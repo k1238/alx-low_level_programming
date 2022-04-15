024ad51a8267e14902e30a80471f474a93070304
k1238 <kalebadino@gmail.com>
2022-04-15 12:35:48 +0300

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numer zero to nine
+ *
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i < 10; i++)
+
+		_putchar(i + '0');
+
+	_putchar ('\n');
+}
