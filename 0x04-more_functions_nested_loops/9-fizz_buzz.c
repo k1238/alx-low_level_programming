f5735a83d9db649c42b6fd9b5a5ae0a600e551c9
k1238 <kalebadino@gmail.com>
2022-04-15 14:03:18 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints Fizz for int divisible by 3 and Buzz for 5
+ * main - prints fizz for int divisible by 3 and Buzz for 5
  *
  * Return: int
  */
+
 int main(void)
 {
 	int i;
@@ -13,24 +14,26 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-				printf("Fizz");
-			}
-			else if (i % 3 != 0 && i % 5 == 0)
-			{
-				printf("Buzz");
-			}
-			else if (i % 3 == 0 && i % 5 == 0)
-			{
-				printf("FizzBuzz");
-			}
-			else 
-			{
-				printf("%d", i);
-			}
-			if (i != 100)
-				putchar(' ');
-			else
-				putchar('\n');
+			printf("Fizz");
 		}
-		return (0);
+		else if (i % 3 != 0 && i % 5 ==0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+			putchar(' ');
+		else
+			putchar('\n');
 	}
+	return (0);
+}
+
+
