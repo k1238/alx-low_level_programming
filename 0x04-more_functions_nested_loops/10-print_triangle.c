f0a7a19eace37d876f6e5dab63f4107065c71e8d
k1238 <kalebadino@gmail.com>
2022-04-15 12:53:26 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,29 +3,29 @@
 /**
  * print_triangle - prints triangle
  * @size: limit
+ *
  * Return: void
  */
 void print_triangle(int size)
 {
-        int i, j;
-
-        if (size > 0)
-        {
+	int i, j;
+	
+	if (siz > 0)
+	{
 		for (i = 0; i < size; i++)
-                        {
-                                for (j = 0; j < size; j++)
-                                {
-                                        if ((size - i - 1) > j)
-                                                _putchar(' ');
-                                        else
-                                                 _putchar('#');
-                                }
-                                        _putchar('\n');
-                                }
-                        
-                        _putchar('\n');
-                }
-        }
-
-
-
+		{
+			for (j = 0; j < size; j++)
+			{
+				if ((size - i - 1) > j)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		 _putchar('\n');
+	}
+}
