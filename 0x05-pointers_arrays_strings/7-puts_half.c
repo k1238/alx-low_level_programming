194e8ecff2ee28cc9b5c31a60e175e482fa29242
k1238 <kalebadino@gmail.com>
2022-04-18 11:46:55 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,13 @@
 #include "main.h"
-
 /**
  * puts_half - prints every other character of a string
  * @str: A prints to an int that will be changed
  *
  * Return: void which means our answer is correct
  */
-
 void puts_half(char *str)
-
 {
-
-	int i, last;
+int i, last;
 
 	i = 0;
 	while (str[i] != '\0')
