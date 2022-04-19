8ffc2ffa1a67ff045bebf36c97d7639066778459
k1238 <kalebadino@gmail.com>
2022-04-19 07:28:25 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
- * main - random password generator for 101-crackme
- *
- * Return: always 0
  */
  int mian(void)
 {
