416258744c64d11c313964c18d3b7694e329fb0c
k1238 <kalebadino@gmail.com>
2022-04-20 11:29:19 -0700

    task 2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
 char *strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *strncpy(char *dest, char *src, int n);
@@ -14,4 +11,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
 
-#endif
+
