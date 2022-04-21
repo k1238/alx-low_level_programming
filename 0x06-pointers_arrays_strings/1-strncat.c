c92ed7392484ceb640d3d9e5337d55ed78ceb20d
k1238 <kalebadino@gmail.com>
2022-04-21 11:07:18 -0700

    task 0

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,22 +10,23 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int c, i;
+        int c, i;
 
-	c = 0;
+        c = 0;
 
-	/*find size of dest array*/
-	while (dest[c])
-		c++;
+        /*find size of dest array*/
+        while (dest[c])
+                c++;
 
-	/**
-	 * src does not need to be null terminated
-	 * if it combines n or more bytes
-	 */
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src[i];
-	/* null terminate dest*/
-	dest[c + i] = '\0';
+        /**
+         * src does not need to be null terminated
+         * if it combines n or more bytes
+         */
+        for (i = 0; i < n && src[i] != '\0'; i++)
+                dest[c + i] = src[i];
+        /* null terminate dest*/
+        dest[c + i] = '\0';
 
-	return (dest);
+        return (dest);
 }
+
