aab5c28dfca261eb6500312cfffa75b8651ba104
k1238 <kalebadino@gmail.com>
2022-04-26 13:02:52 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
-#include  "main.h"
+#include "main.h"
 
 /**
  * _strspn - search a string for a set of bytes
  * @s: source string
  * @accept: accepted string
  *
- * @Return: number of bytes in the init segment
+ * Return: number of bytes in the init segment
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -30,6 +30,3 @@ unsigned int _strspn(char *s, char *accept)
 
 	return (t);
 }
-			
-
-
