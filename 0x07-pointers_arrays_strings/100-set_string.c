903d0fe1ea2868ead0c348a872a915e91a1b460b
k1238 <kalebadino@gmail.com>
2022-04-26 07:01:47 -0700

    task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_string - sets the value of a printer to char
+ * set_string - sets the value of a pointer to char
  * @s: the value to modify
  * @to: the value to assign
  *
@@ -9,5 +9,5 @@
  */
 void set_string(char **s, char *to)
 {
-        *s = to;
+	*s = 0;
 }
