cf2b725dcd265b15da601ad948fa3033b1385eee
k1238 <kalebadino@gmail.com>
2022-04-26 12:41:21 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,6 @@
  * _memset - Fill memory with a constant byte
  * @s: memory area to fill
  * @b: constant byte to fill
- * @n: bytes of memory area to fill
  *
  * Return: the memory area filled
  */
