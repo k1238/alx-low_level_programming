22c51f5bd7fad6a93fe2f072d5306c57e30d5d4c
k1238 <kalebadino@gmail.com>
2022-04-27 10:43:52 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -16,7 +16,9 @@ int _pow_recursion(int x, int y)
 else if (y != 0)
 	return (x * _pow_recursion(x, y - 1)); /* Recursion call*/
 
-			else
-			return (1);
-			}
+	else
+	return (1);
+	}
+	
+			
 			
