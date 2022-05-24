7a35d3e1efd684306164026d6c2d67e2e22821e8
k1238 <kalebadino@gmail.com>
2022-05-24 16:14:49 +0300

    lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef HOLBERTON
-#define HOLBERTON
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -30,6 +34,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-void _printd(int n);
-#endif
 
+#endif /* LISTS_H */
